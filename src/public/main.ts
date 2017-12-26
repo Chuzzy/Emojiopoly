5c7fe94b590e3bb22f5e7773e03b98e015b58520
Chuzzy <thechuzzyguy@gmail.com>
2017-12-26 19:41:29 +0000

    Start some work on displaying property data

--- src/public/main.ts
+++ src/public/main.ts
@@ -23,9 +23,40 @@ let publicFunctions = {
             confirmButtonText: "Yes"
         }).then(result => {
             if (result.value) {
-                swal("💸","You have filed bankruptcy.");
+                swal("💸", "You have filed bankruptcy.");
             }
         });
+    },
+    showPropertyInfo: (name: string, color: string, cost: number, improvement: number, ...fees: number[]) => {
+        let selectedPropertyElement = document.getElementById("selected-property");
+        while (selectedPropertyElement.hasChildNodes()) {
+            selectedPropertyElement.removeChild(selectedPropertyElement.lastChild);
+        }
+        
+        let propertyTitleElement = document.createElement("div");
+        propertyTitleElement.id = "prop-name";
+        propertyTitleElement.textContent = name;
+
+        let improvementText = ["Fee", "👍", "👍👍", "👍👍👍", "👍👍👍👍", "👌"];
+        let transportText = ["Fee", "With 2 transports", "With 3 transports", "With 4 transports"];
+        let priceTableTextElements = ["fee-0", "fee-1", "fee-2", "fee-3", "fee-4", "fee-5"].map((value) => document.getElementById(value + "-text"));
+        let priceTableElements = ["fee-0", "fee-1", "fee-2", "fee-3", "fee-4", "fee-5"].map((value) => document.getElementById(value));
+
+        if (color) {
+            propertyTitleElement.className = color;
+            priceTableElements.forEach((element, index) => {
+                priceTableTextElements[index].innerHTML = improvementText[index];
+                element.innerHTML = fees[index].toString();
+            });
+            document.getElementById("cost-improve-text").innerHTML = "Cost per 👍/👌";
+            document.getElementById("cost-improve").innerHTML = improvement.toString();
+        } else {
+            propertyTitleElement.className = undefined;
+            
+        }
+        
+        document.getElementById("disabled-value").innerText = Math.floor(cost * 0.5).toString();
+        document.getElementById("cost-reenable").innerText = Math.floor(cost * 0.55).toString();
     }
 };
 
