bb707ffe6625d9a5355713c51c98033efd48e3ac
Chuzzy <thechuzzyguy@gmail.com>
2018-01-07 19:30:15 +0000

    Add JSDoc to Square.addOccupant and Square.removeOccupant

--- src/monopoly/Monopoly.ts
+++ src/monopoly/Monopoly.ts
@@ -40,10 +40,18 @@ namespace Monopoly {
     export class Square {
         public occupants: Player[];
 
+        /**
+         * Add a player to this square's occupants.
+         * @param occupant The player to add to this square's occupants.
+         */
         public addOccupant(occupant: Player) {
             this.occupants.push(occupant);
         }
 
+        /**
+         * Remove a player from this square's occupants.
+         * @param occupant The player to remove from this square's occupants.
+         */
         public removeOccupant(occupant: Player) {
             this.occupants.splice(this.occupants.indexOf(occupant), 1);
         }
