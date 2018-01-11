89f529290acfa0c705413fb6e5d6e1d1586979fc
Chuzzy <thechuzzyguy@gmail.com>
2018-01-11 09:30:48 +0000

    Add more functionality to Debt

--- src/monopoly/Debt.ts
+++ src/monopoly/Debt.ts
@@ -1,4 +1,5 @@
 import { MonopolyGame, Player } from "./Monopoly";
+import * as _ from "lodash";
 
 /**
  * A debt to be paid to another player or the bank.
@@ -6,36 +7,56 @@ import { MonopolyGame, Player } from "./Monopoly";
  */
 export class Debt {
     /**
-     * Pays the debt.
-     * @throws Error if the debtor has less money than the debt amount.
+     * The amount of money left to pay.
+     */
+    public amount: number;
+
+    /**
+     * Attempts to pay the debt.
+     * The debt amount will be reduced if the debtor doesn't have enough money.
+     * @throws Error if the debtor has no money.
      */
     public payDebt() {
-        if (this.amount > this.debtor.money) {
+        if (this.debtor.money === 0) {
             // Debt is unpayable
-            throw new Error("Not enough money to pay this debt.");
+            throw new Error("No money to pay this debt.");
+        }
+        else if (this.amount > this.debtor.money) {
+            // Pay as much debt as possible
+            this.amount -= this.debtor.money;
+            this.debtor.money = 0;
+            this.game.messageEventHandler(`${this.debtor} still needs to pay 💷${this.amount} to ${this.creditor || "the bank"}`);
         } else {
-            this.game.messageEventHandler(`${this.debtor} paid ${this.creditor || "the bank"} 💷${this.amount}`);
+            // Pay off the (remaining) debt
+            this.game.messageEventHandler(`${this.debtor} paid ${this.creditor || "the bank"} 💷${this.initialAmount}`);
 
             this.debtor.money -= this.amount;
+
+            // Pay the money to the creditor or into free parking (or no one)
             if (this.creditor !== null)
-                this.creditor.money += this.amount;
+                this.creditor.money += this.initialAmount;
             else if (this.game.houseRules.freeParkingJackpot === "fines")
-                this.game.addToJackpot(this.amount);
+                this.game.addToJackpot(this.initialAmount);
+
+            // Remove the debt
+            _.remove(this.game.unpaidDebts, this);
         }
     }
 
     public toString() {
-        return `${this.debtor} owes ${this.creditor || "the bank"} ${this.amount}`;
+        return `${this.debtor} owes ${this.creditor || "the bank"} ${this.initialAmount}`;
     }
 
     /**
-     * Creates a new debt.
+     * Creates a new debt and adds it to the game's unpaid debts.
      * @param game The game this debt belongs to.
      * @param debtor The player who owes money.
      * @param creditor The player who is receiving the money
      * or null if the money is owed to the bank.
-     * @param amount The amount of money owed.
+     * @param initialAmount The initial amount of money owed.
      */
-    constructor(public readonly game: MonopolyGame, public readonly debtor: Player, public readonly creditor: Player | null, public readonly amount: number) {
+    constructor(public readonly game: MonopolyGame, public readonly debtor: Player, public readonly creditor: Player | null, public readonly initialAmount: number) {
+        this.amount = initialAmount;
+        game.unpaidDebts.push(this);
     }
 }
