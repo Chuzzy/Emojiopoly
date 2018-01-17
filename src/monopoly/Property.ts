6aee63f094d538d5200305ed4cd0b4d2f548db06
Chuzzy <thechuzzyguy@gmail.com>
2018-01-17 08:41:48 +0000

    Add color and rent fields

--- src/monopoly/Property.ts
+++ src/monopoly/Property.ts
@@ -20,12 +20,18 @@ export class Property {
     /**
      * Initialize a new Property instance, specifying the name, price and building costs.
      * @param name The name of the property.
+     * @param color The property's color.
      * @param price The price to purchase the property.
+     * @param rent The rent on this property.
+     * If rent is an array, the rent to be paid is
+     * `rent[buildingsCount]`.
      * @param buildPrice The cost to build on the property.
      */
     constructor(
         public readonly name: string,
+        public readonly color: "brown" | "lightblue" | "pink" | "orange" | "red" | "yellow" | "green" | "blue",
         public readonly price: number,
+        public readonly rent: "transport" | "utility" | number[],
         public readonly buildPrice: number
     ) {
         this.buildingsCount = 0;
